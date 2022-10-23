46d7b3005a99063931c8dbeae7614508ec91f2df
decordel <decordel@student.21-school.ru>
2022-10-23 16:04:24 +0300

    add redis to wordpress

--- srcs/requirements/wordpress/tools/do_work.sh
+++ srcs/requirements/wordpress/tools/do_work.sh
@@ -4,4 +4,8 @@
 wp core install --url="$DOMAIN_NAME" --title="$TITLE"\
 		--admin_user="$WP_ADMIN_USER" --admin_password="$WP_ADMIN_PASS"\
 		--admin_email="$WP_ADMIN_MAIL" --path="/var/www/wordpress"
+
+wp plugin install redis-cache --activate --activate-network --path="/var/www/html"
+wp redis enable --force --path="/var/www/html"
+
 php-fpm7 -F
