a61b4d1005277a06720548e9c7a624659c42069c
decordel <decordel@student.21-school.ru>
2022-10-24 22:47:39 +0300

    fix redis

--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -80,6 +80,7 @@ $table_prefix = 'wp_';
  * @link https://wordpress.org/support/article/debugging-in-wordpress/
  */
 define( 'WP_DEBUG', false );
+define( 'WP_REDIS_HOST', 'redis' );
 
 /* Add any custom values between this line and the "stop editing" line. */
 
--- srcs/requirements/wordpress/tools/do_work.sh
+++ srcs/requirements/wordpress/tools/do_work.sh
@@ -4,8 +4,8 @@
 wp core install --url="$DOMAIN_NAME" --title="$TITLE"\
 		--admin_user="$WP_ADMIN_USER" --admin_password="$WP_ADMIN_PASS"\
 		--admin_email="$WP_ADMIN_MAIL" --path="/var/www/wordpress"
-
-wp plugin install redis-cache --activate --activate-network --path="/var/www/html"
-wp redis enable --force --path="/var/www/html"
+# wp user create $WP_USER $WP_USER@$DOMAIN_NAME --role="author" --user_pass="$WP_PASSWORD" --path="/var/www/wordpress"
+wp plugin install redis-cache --activate --activate-network --path="/var/www/wordpress"
+wp redis enable --force --path="/var/www/wordpress"
 
 php-fpm7 -F
