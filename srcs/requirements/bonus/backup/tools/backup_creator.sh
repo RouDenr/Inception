215def766f069236e9475986deca9d68ccffcdd6
decordel <decordel@student.21-school.ru>
2022-10-25 22:17:51 +0300

    add del old dirs : image:backup

--- srcs/requirements/bonus/backup/tools/backup_creator.sh
+++ srcs/requirements/bonus/backup/tools/backup_creator.sh
@@ -4,7 +4,12 @@ echo "starting backup service every 5 minutes for the database..." >> backup.log
 
 while true;
 do
-	time=$(date '+%Y-%m-%d %H:%M:%S')
+	old_file=$(find . -type d -mtime +5)
+	if [ -z "${old_file}"]; then
+		find . -type d -mtime +5 -exec rm -fr {} \;
+		echo "backups $old_file deleted" >> backup.log
+	fi
+	time=$(date '+%Y_%m_%d_%H:%M:%S')
 
 	backup_folder="backup_${time}"
 	mkdir "/backup/$backup_folder"
@@ -14,6 +19,8 @@ do
 
 	# sleep for 5 minutes
 	sleep 5m
+
+
 done
 
 exit 0
